33814f5b14c114a7f1ff3abd104fb1eebf7b3dde
Matt Clay <matt@mystile.com>
2020-09-16 23:24:55 -0700

    Demo for log backscroll corruption.

---
+++ demo.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+
+runtime_seconds = 300
+result_count = 1500
+result_lines = 10
+
+sleep_seconds = runtime_seconds / result_count
+
+for result in range(1, result_count + 1):
+    print('Result {result}'.format(result=result))
+
+    for line in range(1, result_lines - 1):
+        print('  Line {line}'.format(line=line))
+
+    print()
+
+    sys.stdout.flush()
+
+    time.sleep(sleep_seconds)

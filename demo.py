1660f6239b5bf1225ee78d8b2df4114adbce08d9
Matt Clay <matt@mystile.com>
2020-09-17 10:14:45 -0700

    Simulate 5-minute jobs with output.

---
+++ demo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import time
+
+runtime_seconds = 300
+result_count = 1500
+result_lines = 10
+
+sleep_seconds = runtime_seconds / result_count
+
+for result in range(1, result_count + 1):
+    print('Result {result}'.format(result=result))
+
+    for line in range(1, result_lines - 1):
+        print('  Line {line}'.format(line=line))
+
+    print()
+
+    time.sleep(sleep_seconds)
